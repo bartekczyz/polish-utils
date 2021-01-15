4ff72e6551b0aecfc2e5dba84d1b80616c31f8b1
Bartek Czyż <bartek@czyz.it>
2021-01-15 17:55:53 +0100

    add PESEL utility

---
+++ __tests__/pesel.spec.ts
@@ -0,0 +1,76 @@
+import { PESEL } from '../src'
+
+const validMales = ['02040968991', '03291243431', '46510300036', '99663011115']
+const invalidMales = ['03291243432', '03213192538']
+const validFemales = ['79050821127', '54090462123']
+const invalidFemales = ['79050821128', '54090462124']
+
+describe('pesel', () => {
+    describe('isValid should be true if PESEL is valid', () => {
+        ;[...validMales, ...validFemales].forEach((peselNumber) => {
+            it(`using ${peselNumber}`, () => {
+                const pesel = new PESEL(peselNumber)
+                expect(pesel.isValid).toEqual(true)
+            })
+        })
+    })
+
+    describe('isValid should be false if PESEL is invalid', () => {
+        ;[...invalidMales, ...invalidFemales].forEach((peselNumber) => {
+            it(`using ${peselNumber}`, () => {
+                const pesel = new PESEL(peselNumber)
+                expect(pesel.isValid).toEqual(false)
+            })
+        })
+    })
+
+    describe('getGender should return "male" if PESEL belongs to a male', () => {
+        validMales.forEach((peselNumber) => {
+            it(`using valid ${peselNumber}`, () => {
+                const pesel = new PESEL(peselNumber)
+                expect(pesel.getGender()).toEqual('male')
+            })
+        })
+
+        invalidMales.forEach((peselNumber) => {
+            it(`using invalid ${peselNumber}`, () => {
+                const pesel = new PESEL(peselNumber)
+                expect(pesel.getGender()).toEqual('male')
+            })
+        })
+    })
+
+    describe('getGender should return "female" if PESEL belongs to a female', () => {
+        validFemales.forEach((peselNumber) => {
+            it(`using valid ${peselNumber}`, () => {
+                const pesel = new PESEL(peselNumber)
+                expect(pesel.getGender()).toEqual('female')
+            })
+        })
+        invalidFemales.forEach((peselNumber) => {
+            it(`using valid ${peselNumber}`, () => {
+                const pesel = new PESEL(peselNumber)
+                expect(pesel.getGender()).toEqual('female')
+            })
+        })
+    })
+
+    describe('getDateOfBirth should return date of birth Date object', () => {
+        it(`using valid ${validMales[0]}`, () => {
+            expect(new PESEL(validMales[0]).getDateOfBirth()).toEqual(new Date(1902, 3, 9))
+            expect(new PESEL(validMales[1]).getDateOfBirth()).toEqual(new Date(2003, 8, 12))
+            expect(new PESEL(validMales[2]).getDateOfBirth()).toEqual(new Date(2146, 10, 3))
+            expect(new PESEL(validMales[3]).getDateOfBirth()).toEqual(new Date(2299, 5, 30))
+        })
+    })
+
+    describe('getDateOfBirth should return null if date part is not parsable', () => {
+        const invalidParts = ['abc', '9x9x9x', '12345c']
+
+        invalidParts.forEach((invalidPart) => {
+            it(`using invalid ${invalidPart}`, () => {
+                expect(new PESEL(invalidPart).getDateOfBirth()).toEqual(null)
+            })
+        })
+    })
+})
---
+++ src/pesel/index.ts
@@ -0,0 +1,71 @@
+type Gender = 'male' | 'female'
+
+export class PESEL {
+    private readonly valid: boolean
+
+    constructor(private readonly pesel: string) {
+        this.valid = this.validate()
+    }
+
+    get isValid() {
+        return this.valid
+    }
+
+    getGender(): Gender | null {
+        if (this.pesel.length < 10) {
+            return null
+        }
+
+        const genderDigit = +this.pesel[9]
+
+        if (genderDigit % 2 === 0) {
+            return 'female'
+        }
+
+        return 'male'
+    }
+
+    getDateOfBirth(): Date | null {
+        if (this.pesel.length < 6) {
+            return null
+        }
+
+        let [year, month, day] = this.pesel.match(/.{1,2}/g).map((part) => +part || 0)
+        let baseYear = 1900
+
+        if (month > 20 && month < 40) {
+            baseYear = 2000
+            month -= 20
+        } else if (month > 40 && month < 60) {
+            baseYear = 2100
+            month -= 40
+        } else if (month > 60 && month < 80) {
+            baseYear = 2200
+            month -= 60
+        } else if (month > 80 && month < 99) {
+            baseYear = 1800
+            month -= 80
+        }
+
+        if (!year || !month || !day) {
+            return null
+        }
+
+        return new Date(baseYear + year, --month, day)
+    }
+
+    private validate() {
+        if (!/^[0-9]{11}$/.test(this.pesel)) {
+            return false
+        }
+
+        const weights = [1, 3, 7, 9]
+        const digits = `${this.pesel}`.split('').map((digit) => +digit)
+        const lastDigit = digits.splice(-1)[0]
+        const control =
+            digits.reduce((previous, current, index) => previous + current * weights[index % 4]) %
+            10
+
+        return 10 - (control === 0 ? 10 : control) === lastDigit
+    }
+}
